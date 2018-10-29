b29c5ff3b1b6816f3e7d164dea241ef50db03783
vagrant <vagrant@vagrant.vm>
2018-10-29 04:59:17 +0000

    Created salary table template.

--- model.py
+++ model.py
@@ -35,6 +35,8 @@ class Company(db.Model):
 
         return f"<Company company_id={self.company_id} name={self.name}>"
 
+    def as_dict(self):
+        return {'name': self.name}
 
 
 class Industry(db.Model):
@@ -115,17 +117,18 @@ def connect_to_db(app):
     # Configure to use our PstgreSQL database
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///grow"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.app = app
     db.init_app(app)
 
 
-if __name__ == "__main__":
-    # As a convenience, if we run this module interactively, it will leave
-    # you in a state of being able to work with the database directly.
+# if __name__ == "__main__":
+#     # As a convenience, if we run this module interactively, it will leave
+#     # you in a state of being able to work with the database directly.
 
-    from server import app
-    connect_to_db(app)
-    print("Connected to DB.")   
+#     from server import app
+#     connect_to_db(app)
+#     print("Connected to DB.")   
 
     
 
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session, url_for, jsonify
+from flask import Flask, render_template, redirect, request, flash, session, url_for, jsonify, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import Company, Industry, Interest, Salary, connect_to_db, db
@@ -19,13 +19,41 @@ app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 
 
-@app.route('/form')
-def sdg():
+@app.route('/search')
+def select_company():
+    """Select Company for search."""
+
     form = CompanyForm()
-    return render_template('sdg.html', form=form)
+
+    return render_template('company_search_form.html', form=form)
+
+@app.route('/companies')
+def companydic():
+    """Using for Jquery company name Autocomplete."""
+
+    res = Company.query.all()
+    list_companies = [r.as_dict() for r in res]
+    return jsonify(list_companies)    
 
 
+@app.route('/company_view', methods=['POST'])
+def salary_view():
+    """Create the company's salary table."""
+    form = CompanyForm(request.form)
+    company_name = form.company.data
+    company_name = company_name.upper()
+    company = Company.query.filter_by(name=company_name).first()
 
+    if company != None:
+
+        salary_query = company.salaries
+
+        return render_template('form.html', salary_query=salary_query)
+
+    else:
+
+        flash("Please check the company name.")
+        return redirect("/form")
 
 
 
@@ -36,7 +64,7 @@ if __name__ == "__main__":
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
 
-    # connect_to_db(app)
+    connect_to_db(app)
     # db.create_all()
 
     # Use the DebugToolbar
