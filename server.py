90b23b71ab8f99d418c25335b49bdd70aab65def
vagrant <vagrant@vagrant.vm>
2018-11-02 22:42:17 +0000

    Put a job scraper code in a seperate file.

--- server.py
+++ server.py
@@ -2,14 +2,12 @@ from jinja2 import StrictUndefined
 import json
 from flask import Flask, render_template, redirect, request, flash, session, url_for, jsonify, flash
 from flask_debugtoolbar import DebugToolbarExtension
+import requests
+import os
 
 from model import Company, Industry, Interest, Salary, connect_to_db, db
 from forms import CompanyForm
-from lxml import html, etree
-import requests
-import re
-import argparse
-import os
+from job_listings_scraper import get_job_listings
 
 
 app = Flask(__name__)
@@ -44,7 +42,7 @@ def companydic():
 @app.route("/company_view")
 def create_result_view():
     """Create the company's salary table."""
-  
+    from datetime import datetime; start = datetime.now()
     form = CompanyForm(request.args)
     company_name = form.company.data
     company_name = company_name.upper()
@@ -53,10 +51,14 @@ def create_result_view():
           .joinedload("companies")
           .joinedload("interest")
     ).filter_by(name=company_name).first()
-
+    print('#' * 20, datetime.now() - start) # for checking runtime
     
     job_listings = get_job_listings(company_name)
+    print('#' * 20, datetime.now() - start) # for checking runtime
+
+
     company_infos = get_company_infos(company_name)
+    print('#' * 20, datetime.now() - start) # for checking runtime
 
     if company.industry:
         ranking = get_interest_growth_ranking(company)
@@ -65,6 +67,7 @@ def create_result_view():
     else:
         ranking = None
         industry_name = None
+
 
     if company != None:
 
@@ -80,110 +83,6 @@ def create_result_view():
         flash("Please check the company name.")
         return redirect("/search")
 
-
-def get_job_listings(company_name):
-    """Glassdoor job postings scraper. source : scrapehero."""
- 
-    keyword = company_name
-    place =  "San Francisco Bay area"
-
-    headers = { 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'accept-encoding': 'gzip, deflate, sdch, br',
-                'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6',
-                'referer': 'https://www.glassdoor.com/',
-                'upgrade-insecure-requests': '1',
-                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
-                'Cache-Control': 'no-cache',
-                'Connection': 'keep-alive'
-    }
-
-    location_headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.01',
-        'accept-encoding': 'gzip, deflate, sdch, br',
-        'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6',
-        'referer': 'https://www.glassdoor.com/',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
-    }
-    data = {"term": place,
-            "maxLocationsToReturn": 10}
-
-    location_url = "https://www.glassdoor.co.in/findPopularLocationAjax.htm?"
-    try:
-        # Getting location id for search location
-        print("Fetching location details")
-        location_response = requests.post(location_url, headers=location_headers, data=data).json()
-        place_id = location_response[0]['locationId']
-        job_litsting_url = 'https://www.glassdoor.com/Job/jobs.htm'
-        # Form data to get job results
-        data = {
-            'clickSource': 'searchBtn',
-            'sc.keyword': keyword,
-            'locT': 'C',
-            'locId': place_id,
-            'jobType': ''
-        }
-
-        job_listings = []
-        if place_id:
-            response = requests.post(job_litsting_url, headers=headers, data=data)
-            parser = html.fromstring(response.text)
-            # Making absolute url 
-            base_url = "https://www.glassdoor.com"
-            parser.make_links_absolute(base_url)
-
-            XPATH_ALL_JOB = '//li[@class="jl"]'
-            XPATH_NAME = './/a/text()'
-            XPATH_JOB_URL = './/a/@href'
-            XPATH_LOC = './/span[@class="subtle loc"]/text()'
-            XPATH_COMPANY = './/div[@class="flexbox empLoc"]/div/text()'
-            XPATH_SALARY = './/span[@class="green small"]/text()'
-            XPATH_RATING = './/span[@class="compactStars "]/text()'
-
-
-            listings = parser.xpath(XPATH_ALL_JOB)
-            for job in listings:
-                raw_job_name = job.xpath(XPATH_NAME)
-                raw_job_url = job.xpath(XPATH_JOB_URL)
-                raw_lob_loc = job.xpath(XPATH_LOC)
-                raw_company = job.xpath(XPATH_COMPANY)
-                raw_salary = job.xpath(XPATH_SALARY)
-                raw_rating = job.xpath(XPATH_RATING)
-
-                # Cleaning data
-                job_name = ''.join(raw_job_name).strip('–') if raw_job_name else None
-                job_location = ''.join(raw_lob_loc) if raw_lob_loc else None
-                raw_state = re.findall(",\s?(.*)\s?", job_location)
-                state = ''.join(raw_state).strip()
-                raw_city = job_location.replace(state, '')
-                city = raw_city.replace(',', '').strip()
-                company = ''.join(raw_company).replace('–','')
-                salary = ''.join(raw_salary).strip()
-                rating = ''.join(raw_rating).strip()
-                job_url = raw_job_url[0] if raw_job_url else None
-
-                jobs = {
-                    "Name": job_name,
-                    "Company": company,
-                    "State": state,
-                    "City": city,
-                    "Salary": salary,
-                    "Location": job_location,
-                    "Url": job_url,
-                    "Rating" : rating
-                }
-                job_listings.append(jobs)
-
-            return job_listings
-        else:
-            print("location id not available")
-
-    except:
-        print("Failed to load locations")
-
-
 # @app.route("/job_listings.json")
 # def _job_listings_convert_to_json():
 #     """Convert a list to JSON."""
@@ -194,7 +93,7 @@ def get_job_listings(company_name):
 
 
 @app.route("/interest.json") 
-def get_interest():
+def get_interest_chart():
     """Create interest list using sqlalchemy for chart."""
 
     company_name = request.args.get("company_name")
@@ -292,13 +191,17 @@ def get_company_infos(company_name):
     response = requests.get(search_url, headers=headers, params=params)
     response.raise_for_status()
     search_results = response.json()
-    print(search_results)
 
-    company_desc = search_results['entities']['value'][0]['description']
-    company_img = search_results['entities']['value'][0]['image']['thumbnailUrl']
+    if search_results:
 
-    return (company_desc, company_img)
+        company_desc = search_results['entities']['value'][0]['description']
+        company_img = search_results['entities']['value'][0]['image']['thumbnailUrl']
 
+        return (company_desc, company_img)
+
+    else:
+
+        return (None, None)
 
 
 
