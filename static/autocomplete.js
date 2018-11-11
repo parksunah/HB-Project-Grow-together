b62de4cf1b1d5778be2c179cb6d162764d45d3d7
vagrant <vagrant@vagrant.vm>
2018-11-11 06:55:19 +0000

    Fixed the autocomplete on the top layer.

--- static/autocomplete.js
+++ static/autocomplete.js
@@ -1,17 +1,19 @@
 "use strict";
 
     $(document).ready(function () {
-    const companies=[];
-    function loadCompanies(){
-        $.getJSON('/companies', function(data, status, xhr){
-            for (let i = 0; i < data.length; i++ ) {
-                companies.push(data[i].name);
-            }
-    });
-    };
-    loadCompanies();
-    $("#company").autocomplete({
-                    source: companies, 
-                    minLength:3
-                });
+        
+        const companies=[];
+        function loadCompanies(){
+            $.getJSON('/companies', function(data, status, xhr){
+                for (let i = 0; i < data.length; i++ ) {
+                    companies.push(data[i].name);
+                }
+            });
+        };
+        
+        loadCompanies();
+        $("#company").autocomplete({
+                        source: companies, 
+                        minLength:3
+        });
     });
