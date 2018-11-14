bf4a31a7a2319728a6825b34e30a731398dd5836
vagrant <vagrant@vagrant.vm>
2018-11-14 18:05:37 +0000

    Modified news title as text-overflow: ellipsis.

--- static/news.js
+++ static/news.js
@@ -23,52 +23,51 @@ document.getElementById("myChart").onclick = function(evt){
                     
                     for (let r of response) {
 
-                    const newsImg = r.urlToImage;
-                    if (newsImg === "null") {
-                        newsImg = "/static/images/placeholder.png";
-                    }
+                        const newsImg = r.urlToImage;
+                        
+                        if (newsImg === null) {
+                            newsImg = "./static/images/placeholder.png";
+                        }
 
-                    console.log(newsImg);
+                        // card news design source ::: https://www.louistiti.fr/tutoriel-html5-css3-carte-article-ui/33
+                        const article = 
 
-                    // card news design source ::: https://www.louistiti.fr/tutoriel-html5-css3-carte-article-ui/33
-                    const article = 
-
-                    `
-                     <span>
-                        <div class="card">
-                            <div class="card-header"><img id="news-img" src="${newsImg}">
-                                <div class="card-header-mask">
+                        `
+                         <span>
+                            <div class="card">
+                                <div class="card-header"><img id="news-img" src="${newsImg}">
+                                    <div class="card-header-mask">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="card-body">
-                                <div class="card-body-header">
-                                    <div class="card-body-header-category">${r.publishedAt}</div>
-                                    <h1>${r.title}</h1>
-                                    <p class="card-body-header-sentence">
-                                        ${r.source.name}</span>
+                                <div class="card-body">
+                                    <div class="card-body-header">
+                                        <div class="card-body-header-category">${r.publishedAt}</div>
+                                        <h1>${r.title}</h1>
+                                        <p class="card-body-header-sentence">
+                                            ${r.source.name}</span>
+                                        </p>
+                                    </div>
+                                    <p class="card-body-description">
+                                        ${r.description}
                                     </p>
-                                </div>
-                                <p class="card-body-description">
-                                    ${r.description}
-                                </p>
-                                <div class="card-body-footer">
-                                    <a href="${r.url}" target="_blank"> Read More.. </a>
+                                    <div class="card-body-footer">
+                                        <a href="${r.url}" target="_blank"> Read More.. </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </span>
-                    `
-                    newsList.push(article);
-                    
+                        </span>
+                        `
+                        newsList.push(article);
+                        
+                        }
+
+                        // console.log(newsList.length);
+                        $('#news-article').html(newsList);
+                        // console.log(response);
+
                     }
-
-                    // console.log(newsList.length);
-                    $('#news-article').html(newsList);
-                    // console.log(response);
-
-                }
                 
-    });
+            });
         };
 
 
